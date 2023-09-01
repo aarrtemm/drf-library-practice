cf360fba0acc920bae68185394f237c5b9a3ee8d
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-09-01 17:57:25 +0300

    Create simple view for Borrowing model

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer
+
+
+class BorrowingView(
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    serializer_class = BorrowingSerializer
+    queryset = Borrowing.objects.all()
+    permission_classes = [IsAuthenticated]
