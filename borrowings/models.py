eb77d7893e6887e67043abbb1ba3be5575f03874
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-09-01 17:28:22 +0300

    Wrote model for Borrowing

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from library.models import Book
+from library_service import settings
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} | {self.book} | {self.borrow_date}"
