e07596bc808709a3dd63a921ffe7d2aaafd1deaf
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-09-01 17:59:49 +0300

    Initialize endpoints for borrowing

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,1 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from borrowings.views import BorrowingView
+
 app_name = "borrowing"
+
+router = routers.DefaultRouter()
+router.register("borrowings", BorrowingView)
+
+urlpatterns = [path("", include(router.urls))]
