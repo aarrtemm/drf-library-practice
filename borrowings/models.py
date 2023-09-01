be8bc3bee765f6d9417b15df69951a9090a5d158
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-09-01 18:08:04 +0300

    Added parametrs for fields & wrote validate_future_date function

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,13 +1,25 @@
+from datetime import datetime
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from library.models import Book
 from library_service import settings
 
 
+def validate_future_date(value):
+    if value < datetime.now().date():
+        raise ValidationError("Date cannot be in the past.")
+
+
 class Borrowing(models.Model):
-    borrow_date = models.DateField(auto_now_add=True)
-    expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
+    borrow_date = models.DateField(
+        auto_now_add=True,
+        validators=[validate_future_date],
+    )
+    expected_return_date = models.DateField(validators=[validate_future_date])
+    actual_return_date = models.DateField(
+        null=True, blank=True, validators=[validate_future_date]
+    )
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
