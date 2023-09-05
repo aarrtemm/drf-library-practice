5575ba0006f3b074b8aa9f2e2de8c60fd82d9b95
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-09-05 14:19:28 +0300

    Initialized borrowing list serializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -12,5 +12,18 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "actual_return_date",
             "book",
-            "user",
+        )
+
+
+class BorrowingListSerializer(BorrowingSerializer):
+    book = serializers.SlugRelatedField(source="book", many=False, read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
         )
