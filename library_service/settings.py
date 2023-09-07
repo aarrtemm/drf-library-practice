49c341592f1222ed8b83b9bb9161bdc537197d28
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-09-07 15:27:59 +0300

    Installed and tuned a django-filter

--- library_service/settings.py
+++ library_service/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "library",
     "rest_framework",
     "rest_framework_simplejwt",
+    "django_filters",
     "user",
     "borrowings",
     "debug_toolbar",
@@ -130,7 +131,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
 SIMPLE_JWT = {
