2bab05a69875881643492af801075987e72ca812
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-09-07 15:32:27 +0300

    Tuned filtering by user_id & is_active

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,5 @@
 from django.db import transaction
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, status
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
@@ -21,6 +22,8 @@ class BorrowingView(
 ):
     serializer_class = BorrowingSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["is_active", "user_id"]
 
     def get_queryset(self):
         if self.request.user.is_staff:
